ee4b4153eec2cc3f01b24abb2a74ae605938c33b
Adriana Brandan <145227272+AdriBrandy@users.noreply.github.com>
2024-09-10 00:37:11 -0300

    Resolucion clase 7

--- Clase-7-Js/tareas.js
+++ Clase-7-Js/tareas.js
@@ -11,3 +11,15 @@ function imprimir() {
 }
 
 imprimir();
+
+//RESULTADO: 
+
+// 1
+// 4
+// 3
+// 2
+
+//console.log(1);: Se ejecuta inmediatamente y muestra 1.
+//setTimeout(function() { console.log(2); }, 1000);: Programa una función para que se ejecute después de 1 segundo (1000 milisegundos). Sin embargo, no se ejecuta inmediatamente, solo se agenda.
+// setTimeout(function() { console.log(3); }, 0);: Programa otra función para ejecutarse lo antes posible (después de 0 milisegundos). Aunque el tiempo es 0, debido a la naturaleza asíncrona del setTimeout, esta función se colocará en la cola de tareas, y se ejecutará después de que el código síncrono actual termine.
+// console.log(4);: Se ejecuta inmediatamente después y muestra
